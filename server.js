3c9114ddbeac3dd0ed6797dd2fb15c04655b6e24
surendar <gurusurendar99@gmail.com>
2021-04-15 01:42:14 +0530

    second commit

--- server.js
+++ server.js
@@ -43,4 +43,5 @@ app.use((err,req,res,next)=>{
 })
 
 
-app.listen(process.env.port,()=>console.log("server is on",process.env.port));
+app.listen(process.env.PORT||8080)
+//app.listen(process.env.port,()=>console.log("server is on",process.env.port));
